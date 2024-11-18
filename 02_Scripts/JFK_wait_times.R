a224ad4a846803a21dd5b2bf91691d74badc7fb3
Andy Murtha <james.a.murtha@gmail.com>
2024-11-17 20:57:40 -0500

    Update JFK_wait_times.R
    Updated variables, completed function call and testing in loop

--- 02_Scripts/JFK_wait_times.R
+++ 02_Scripts/JFK_wait_times.R
@@ -1,7 +1,5 @@
 # Script in Process
 
-## TODO: Make function out of code so it can be called in an automated script on AWS
-## TODO: Ensure that insert code works in DuckDB database data process
 ## TODO (LOW): Refactor text clenaup, and research more efficient tag scraping off of page
 ## TODO (LOW): Fix chrome driver issues
 
@@ -20,88 +18,112 @@ library(lubridate)
 library(stringr)
 library(tidyr)
 
-
 # setup
 wdman::selenium()
-selenium_object <- selenium(retcommand = T, check = F)
+# seleniumCommand <- selenium(retcommand = T, check = F)
+# seleniumCommand
 # binman::list_versions("chromedriver")
 
-# firefox
-remote_driver <- rsDriver(browser = "firefox",
-                          chromever = NULL,
-                          verbose = T,
-                          port = free_port())
+# Database Connection ----
 
-# Access Page
-brow <- remote_driver[["client"]]
-brow$open()
-brow$navigate("https://www.jfkairport.com")
+con <- dbConnect(duckdb::duckdb(), dbdir = "02_Data/tsa_app.duckdb", read_only = FALSE)
 
-# Scrape Page
-h <- brow$getPageSource()
-h <- read_html(h[[1]])
-results <- h |> 
-  html_elements('.av-responsive-table') |> 
-  html_table(fill = TRUE) |> 
-  dplyr::bind_rows()
-
-# Tranform Data
-JFK_data <- results |> 
-  mutate(airport = 'JFK',
-        # General Wait Time
-        wait_time = results$`General Line` |> 
-           str_remove_all("                                ") |> 
-           str_remove("                             ") |> 
-           str_remove_all('\n') |> str_sub(13) |> 
-           str_remove("min") |> 
-           as.numeric() |> 
-           suppressWarnings(),
-        # TSA Pre check Wait Time
-        wait_time_pre_check = results$`TSA Pre✓ Line` |> 
-           str_remove_all("          ") |> 
-           str_remove("         ") |> 
-           str_remove_all("  ") |> 
-           str_remove_all('\n') |> 
-           str_sub(14) |> 
-           str_remove('min') |> 
-           as.numeric() |> 
-           suppressWarnings(),
-        # DateTime
-        datetime = lubridate::now(tzone = 'EST'),
-        # Date
-        date = lubridate::today(),
-        # Time Rounded to Minute
-        time = Sys.time() |> 
-            with_tz(tzone = "America/New_York") |> 
-            floor_date(unit = "minute"),
-        timezone = "America/New_York") |>
-  # Rename CheckPoint column
-  rename(checkpoint = `Terminal...1`) |>
-  # Remove unnecessary columns
-  select(-(2:5)) |> 
-  # Reorder remaining columns
-  select(airport, checkpoint, datetime, date, time, timezone, wait_time,
-         wait_time_pre_check)
+# Function to scrape and store TSA checkpoint wait times
+scrape_tsa_data_jfk <- function() {
   
+  # firefox
+  remote_driver <- rsDriver(browser = "firefox",
+                            chromever = NULL,
+                            verbose = T,
+                            port = free_port())
+  
+  
+  # Access Page
+  brow <- remote_driver[["client"]]
+  # brow$open()
+  brow$navigate("https://www.jfkairport.com")
+  
+  
+  # Scrape Page
+  h <- brow$getPageSource()
+  h <- read_html(h[[1]])
+  results <- h |> 
+    html_elements('.av-responsive-table') |> 
+    html_table(fill = TRUE) |> 
+    dplyr::bind_rows()
+  
+  # Tranform Data
+  JFK_data <- results |> 
+    mutate(airport = 'JFK',
+           # General Wait Time
+           wait_time = results$`General Line` |> 
+             str_remove_all("                                ") |> 
+             str_remove("                             ") |> 
+             str_remove_all('\n') |> str_sub(13) |> 
+             str_remove("min") |> 
+             as.numeric() |> 
+             suppressWarnings(),
+           # TSA Pre check Wait Time
+           wait_time_pre_check = results$`TSA Pre✓ Line` |> 
+             str_remove_all("          ") |> 
+             str_remove("         ") |> 
+             str_remove_all("  ") |> 
+             str_remove_all('\n') |> 
+             str_sub(14) |> 
+             str_remove('min') |> 
+             as.numeric() |> 
+             suppressWarnings(),
+           # DateTime
+           datetime = lubridate::now(tzone = 'EST'),
+           # Date
+           date = lubridate::today(),
+           # Time Rounded to Minute
+           time = Sys.time() |> 
+             with_tz(tzone = "America/New_York") |> 
+             floor_date(unit = "minute"),
+           timezone = "America/New_York") |>
+    # Rename CheckPoint column
+    rename(checkpoint = `Terminal...1`) |>
+    # Remove unnecessary columns
+    select(-(2:5)) |> 
+    # Reorder remaining columns
+    select(airport, checkpoint, datetime, date, time, timezone, wait_time,
+           wait_time_pre_check)
+  
+  
+  # Assign to Global Environment
+  assign("JFK_data", JFK_data, envir = .GlobalEnv) 
+  
+  
+  # Insert observations into tsa_wait_times table
+  dbAppendTable(con, name = "tsa_wait_times", value = JFK_data)
+  
+  
+  # Cleanup to rerun
+  print(glue("session has run successfully ", format(Sys.time(), "%a %b %d %X %Y")))
+  rm(results)
+  rm(JFK_data, envir = .GlobalEnv)
+  rm(h)
+  rm(brow)
+  
+  remote_driver$close()
+}
 
-# Assign to Global Environment
-assign("JFK_data", JFK_data, envir = .GlobalEnv) 
+# purrr::slowly(scrape_tsa_data, rate = rate_delay(pause = 60), quiet = FALSE)
 
+i <- 1
 
-# Insert observations into tsa_wait_times table
-dbAppendTable(con, name = "tsa_wait_times", value = JFK_data)
-
-
-# Cleanup to rerun
-print(glue("session has run successfully ", format(Sys.time(), "%a %b %d %X %Y")))
-rm(h)
-rm(results)
-rm(JFK_data)
-rm(brow)
-rm(selenium_object)
-
+for (i in 1:5) {
+  p1 <- lubridate::ceiling_date(Sys.time(), unit = "minute")
+    print(Sys.time())
+    scrape_tsa_data_jfk()
+  theDelay <- as.numeric(difftime(p1,Sys.time(),unit="secs"))
+  Sys.sleep(max(0, theDelay))
+  
+  i <- i + 1
+}
 
 # Close the server
+# rm(seleniumCommand)
 remote_driver$server$stop()
 rm(remote_driver)
-
