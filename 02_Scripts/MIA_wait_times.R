707ee10824342a1bccc148e4d98e0bb4b180e180
Andy Murtha <james.a.murtha@gmail.com>
2024-11-21 18:18:02 -0500

    update temp, mia_transform, add remaining code
    Updated temp object with last function, changed object to MIA_transform, updated mutate within that object, added the rest of the code, tested the code, updated loop to terminate instead of running until the next minute on last iteration.

--- 02_Scripts/MIA_wait_times.R
+++ 02_Scripts/MIA_wait_times.R
@@ -50,7 +50,7 @@ scrape_tsa_data_clt <- function() {
     html_elements(".ag-header-cell-text") |> 
     html_text2()
   
-  temp <- headers[[5]]
+  temp <- last(headers)
   headers <- c(temp, headers) |> 
     head(-1)
   
@@ -70,23 +70,121 @@ scrape_tsa_data_clt <- function() {
   headers <- rep(headers, len = length(checkpoint_data))
   mia_tbl <- tibble(headers, checkpoint_data)
   
-  MIA_data <- mia_tbl |> 
+  MIA_transform <- mia_tbl |> 
     mutate(row = rep(1:(nrow(mia_tbl) / 5), each = 5)) |> 
     pivot_wider(names_from = headers, values_from = checkpoint_data) |>  
     select(-row) |> 
     mutate(General = case_when(str_detect(General, pattern = " / ") ~ str_remove_all(str_split_i(General, pattern = "/", 2), "\\D"), 
-                                (General == "Closed" ~ "N/A"),
+                                (General == "Closed" ~ "NA"),
+                                (General == "N/A" ~ "NA"),
                                 TRUE ~ General), 
            Priority = case_when(str_detect(Priority, pattern = " / ") ~ str_remove_all(str_split_i(Priority, pattern = "/", 2), "\\D"),
-                                (Priority == "Closed" ~ "N/A"),
+                                (Priority == "Closed" ~ "NA"),
+                                (Priority == "N/A" ~ "NA"),
                                 TRUE ~ Priority),
            `TSA-Pre` = case_when(str_detect(`TSA-Pre`, pattern = " / ") ~ str_remove_all(str_split_i(`TSA-Pre`, pattern = "/", 2), "\\D"), 
-                                (`TSA-Pre` == "Closed" ~ "N/A"),
+                                (`TSA-Pre` == "Closed" ~ "NA"),
+                                (`TSA-Pre` == "N/A" ~ "NA"),
                                 TRUE ~ `TSA-Pre`),
            Clear = case_when(str_detect(Clear, pattern = " / ") ~ str_remove_all(str_split_i(Clear, pattern = "/", 2), "\\D"),
-                                (Clear == "Closed" ~ "N/A"),
+                                (Clear == "Closed" ~ "NA"),
+                                (Clear == "N/A" ~ "NA"),
                                 TRUE ~ Clear)
-           )
+           ) |> 
+    mutate(across(c(General:Clear), as.numeric)) |> suppressWarnings()
+  
+    # Create tibble for data insertion
+  if(!exists("MIA_data", envir = .GlobalEnv)) {
+    MIA_data <- tibble(airport = character(),
+                       checkpoint = character(),
+                       datetime = lubridate::ymd_hms(tz = 'EST'),
+                       date = lubridate::ymd(),
+                       time = lubridate::POSIXct(tz = 'EST'),
+                       timezone = character(),
+                       wait_time = numeric(),
+                       wait_time_priority = numeric(),
+                       wait_time_pre_check = numeric(),
+                       wait_time_clear = numeric()
+                       )
+  } else {
+    MIA_data <- get("MIA_data", envir = .GlobalEnv)
+  }
   
   
+  # Insert data into tibble
+  # Prepare data with airport code, date, time, timezone, and wait times
+  MIA_data <- rows_append(MIA_data, tibble(
+    airport = "MIA",
+    checkpoint = MIA_transform$Checkpoint,
+    datetime = lubridate::now(tzone = 'EST'),
+    date = lubridate::today(),
+    time = Sys.time() |> 
+      with_tz(tzone = "America/New_York") |> 
+      floor_date(unit = "minute"),
+    # time = lubridate::now(tzone = 'EST') |>
+    # floor_date(unit = "minute") |>
+    # with_tz('EST'),
+    # format("%H:%M:%S"),
+    # hms::new_hms(),
+    timezone = "America/New_York",
+    wait_time = MIA_transform$General,  # Assume this is a list of wait times for each checkpoint
+    wait_time_priority = MIA_transform$Priority,
+    wait_time_pre_check = MIA_transform$`TSA-Pre`,
+    wait_time_clear = MIA_transform$Clear
+  ))
+
+  
+  assign("MIA_data", MIA_data, envir = .GlobalEnv)  
+  
+  dbAppendTable(con, name = "tsa_wait_times", value = MIA_data)
+  
+  print(glue("session has run successfully ", format(Sys.time(), "%a %b %d %X %Y")))
+  rm(url)
+  rm(h)
+  rm(headers)
+  rm(temp)
+  rm(checkpoint_data)
+  rm(mia_tbl)
+  rm(MIA_transform)
+  rm(MIA_data, envir = .GlobalEnv)
+  
+  brow$close()
+  rm(brow)
+  
+  remote_driver$server$stop()
+  rm(remote_driver)
+  gc() 
+}  
+
+
+# Test Loop ----
+i <- 1
+
+for (i in 1:5) {
+  p1 <- lubridate::ceiling_date(Sys.time(), unit = "minute")
+  
+  print(glue(i, "  ", format(Sys.time())))
+  
+  scrape_tsa_data_mia()
+  
+  theDelay <- as.numeric(difftime(p1,Sys.time(),unit="secs"))
+  
+  i <- i + 1
+  
+  if(i == 6) {
+    break()
+  } else {
+    Sys.sleep(max(0, theDelay))
+  }
+  
+}
+
+
+# Cleanup ----
+rm(i)
+rm(p1)
+rm(theDelay)
+dbDisconnect(con)
+rm(con)
+rm(scrape_tsa_data_mia)
 }  
