37525c992bd5c148821b25bc378f425e5eea9bc6
Andy Murtha <james.a.murtha@gmail.com>
2024-11-22 13:53:10 -0500

    Update MCO_wait_times.R

--- 02_Scripts/MCO_wait_times.R
+++ 02_Scripts/MCO_wait_times.R
@@ -1,27 +1,29 @@
 # install.packages(c("DBI", "polite", "rvest", "RSelenium", "tidyverse", "duckdb", 
 #  "lubridate", "magrittr", glue", "here"))
 
-library(polite, verbose = FALSE, warn.conflicts = FALSE)
-library(rvest, verbose = FALSE, warn.conflicts = FALSE)
-library(RSelenium, verbose = FALSE, warn.conflicts = FALSE)
-library(duckdb, verbose = FALSE, warn.conflicts = FALSE)
-library(lubridate, verbose = FALSE, warn.conflicts = FALSE)
-library(magrittr, verbose = FALSE, warn.conflicts = FALSE)
-library(glue, verbose = FALSE, warn.conflicts = FALSE)
-library(DBI, verbose = FALSE, warn.conflicts = FALSE)
-library(tidyverse, verbose = FALSE, warn.conflicts = FALSE)
-library(here, verbose = FALSE, warn.conflicts = FALSE)
+# library(polite, verbose = FALSE, warn.conflicts = FALSE)
+# library(rvest, verbose = FALSE, warn.conflicts = FALSE)
+# library(RSelenium, verbose = FALSE, warn.conflicts = FALSE)
+# library(duckdb, verbose = FALSE, warn.conflicts = FALSE)
+# library(lubridate, verbose = FALSE, warn.conflicts = FALSE)
+# library(magrittr, verbose = FALSE, warn.conflicts = FALSE)
+# library(glue, verbose = FALSE, warn.conflicts = FALSE)
+# library(DBI, verbose = FALSE, warn.conflicts = FALSE)
+# library(tidyverse, verbose = FALSE, warn.conflicts = FALSE)
+# library(here, verbose = FALSE, warn.conflicts = FALSE)
 
 
 # Database Connection ----
 
-con <- dbConnect(duckdb::duckdb(), dbdir = "02_Data/tsa_app.duckdb", read_only = FALSE)
+# con <- dbConnect(duckdb::duckdb(), dbdir = "02_Data/tsa_app.duckdb", read_only = FALSE)
 
 
 # Script Function ----
 
 # Function to scrape and store TSA checkpoint wait times
 scrape_tsa_data_mco <- function() {
+  
+  print(glue("kickoff MCO scrape ", format(Sys.time(), "%a %b %d %X %Y")))
   
     # Define URL and initiate polite session
     url <- "https://flymco.com/security/"  # Update with the actual URL
@@ -49,13 +51,14 @@ scrape_tsa_data_mco <- function() {
     # Click Accept All Cookie Button
     Sys.sleep(3)
     button <- brow$findElement(using = 'id', "CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll")
-    if(button$isElementDisplayed() == TRUE) {button$clickElement()}
+    if(button$isElementDisplayed() == TRUE) {button$clickElement()} 
 
-    Sys.sleep(1)
+    # Sys.sleep(1)
     gates <- h |> 
-      html_elements("h5.css-1osfado-headings-H5-WidgetContainer-WidgetTitle-SecurityWaitTimesCard-SWTCardTitle.e1v0x5ca2") |> 
-      html_text2() |> 
-      magrittr::extract(c(1:3))
+      # h5.css-1osfado-headings-H5-WidgetContainer-WidgetTitle-SecurityWaitTimesCard-SWTCardTitle.e1v0x5ca2
+      html_elements("h5.css-1osfado-headings-H5-WidgetContainer-WidgetTitle-SecurityWaitTimesCard-SWTCardTitle.e1v0x5ca2") |>
+      html_text2() 
+      # |> magrittr::extract(c(1:3))
 
 
     wait_time <- h |> 
@@ -65,11 +68,11 @@ scrape_tsa_data_mco <- function() {
       as.numeric()
 
 
-    wait_time_pre_check <- NA
-    
+    wait_time_pre_check <- rep(NA, length(wait_time))
+
     # Comment Out Checks from Testing
-    #print(glue("wait time length ", {length(wait_time)}))
-    #print(glue("gates length ", {length(gates)}))
+    # print(glue("wait time length ", {length(wait_time)}))
+    # print(glue("gates length ", {length(gates)}))
 
     # Comment Out Checks from Testing
     # Check to make Sure that TSA CheckPoint and Time have the same length
@@ -87,7 +90,9 @@ scrape_tsa_data_mco <- function() {
                          time = lubridate::POSIXct(tz = 'EST'),
                          timezone = character(),
                          wait_time = numeric(),
-                         wait_time_pre_check = numeric())
+                         wait_time_priority = numeric(),
+                         wait_time_pre_check = numeric(),
+                         wait_time_clear = numeric())
     } else {
       MCO_data <- get("MCO_data", envir = .GlobalEnv)
     }
@@ -110,7 +115,9 @@ scrape_tsa_data_mco <- function() {
       # hms::new_hms(),
       timezone = "America/New_York",
       wait_time = wait_time,  # Assume this is a list of wait times for each checkpoint
-      wait_time_pre_check = wait_time_pre_check
+      wait_time_priority = NA,
+      wait_time_pre_check = wait_time_pre_check,
+      wait_time_clear = NA
     ))
     
     assign("MCO_data", MCO_data, envir = .GlobalEnv)  
@@ -131,28 +138,28 @@ scrape_tsa_data_mco <- function() {
     
     remote_driver$server$stop()
     rm(remote_driver)
-    gc()  
+    # gc()  
 }
 
 
 # Test Loop ----
-i <- 1
-
-for (i in 1:5) {
-  p1 <- lubridate::ceiling_date(Sys.time(), unit = "minute")
-  print(glue(i, "  ", format(Sys.time())))
-  scrape_tsa_data_mco()
-  theDelay <- as.numeric(difftime(p1,Sys.time(),unit="secs"))
-  Sys.sleep(max(0, theDelay))
-  
-  i <- i + 1
-}
+# i <- 1
+# 
+# for (i in 1:5) {
+#   p1 <- lubridate::ceiling_date(Sys.time(), unit = "minute")
+#   print(glue(i, "  ", format(Sys.time())))
+#   scrape_tsa_data_mco()
+#   theDelay <- as.numeric(difftime(p1,Sys.time(),unit="secs"))
+#   Sys.sleep(max(0, theDelay))
+#   
+#   i <- i + 1
+# }
 
 
 # Cleanup ----
-rm(i)
-rm(p1)
-rm(theDelay)
-dbDisconnect(con)
-rm(con)
-rm(scrape_tsa_data_mco)
+# rm(i)
+# rm(p1)
+# rm(theDelay)
+# dbDisconnect(con)
+# rm(con)
+# rm(scrape_tsa_data_mco)
