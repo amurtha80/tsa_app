91bd52e743c22e344a7e5eedb460d5bec16bedd1
amurtha80 <james.a.murtha@gmail.com>
2024-12-27 21:37:18 -0500

    Added notes on 15 minute groupings and added notes on plotting chart.

--- 02_Scripts/xx_chart_wait_times.R
+++ 02_Scripts/xx_chart_wait_times.R
@@ -15,10 +15,12 @@ here::here()
 
 # TODO ----
 # 1. Potentially use `tibbletime` package function collapse_by to get into 
-#     15 minute groupings
+#     15 minute groupings. Also lubridate's ceiling_date function might work
+#     well (https://forum.posit.co/t/how-to-aggregate-15-min-data-to-30-45-and-so-on/136554)
 # 2. Take 15 minute time intervals and also group by airport to get 15 minute
 #     intervals by airport
 # 3. Plot bar chart of 15 minute intervals with a 60 minute buffer on each side,
 #     totaling 9 bars in the bar chart. This is an average time for all time
 #     slots for that day/time combination
-# 4. Plot horizontal line in 15 minute intervals 
+# 4. Plot horizontal line in 15 minute intervals as the maximum time with a 60 
+#     minute buffer on each side
