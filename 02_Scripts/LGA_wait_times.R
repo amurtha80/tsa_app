2f5426c4cfb3a34cb476a2d4c6dce483771af1b2
amurtha80 <james.a.murtha@gmail.com>
2025-03-28 10:17:45 -0400

    updated mutate to attempt to remove NA coercion based on Post help script, added suppressWarnings() to hide warning

--- 02_Scripts/LGA_wait_times.R
+++ 02_Scripts/LGA_wait_times.R
@@ -51,8 +51,11 @@ scrape_tsa_data_lga <- function() {
              str_remove_all('\n') |> 
              str_remove_all('min') |>
              str_remove_all('GeneralLine')) |> 
-    mutate(wait_time = case_when(wait_time == "NoWait" ~ "0", TRUE ~ wait_time) |>
-             as.numeric()) |> 
+    mutate(wait_time = case_when(wait_time == "NoWait" ~ "0", 
+                                 wait_time == "" ~ NA,
+                                 TRUE ~ wait_time),
+           wait_time = as.numeric(wait_time)) |> 
+    suppressWarnings() |> 
     # TSA Pre check Wait Time
     mutate(wait_time_pre_check = results$`TSA Pre✓ Line` |> 
              str_remove_all(' ') |> 
@@ -60,8 +63,10 @@ scrape_tsa_data_lga <- function() {
              str_remove_all('min') |> 
              str_remove_all('TSAPre✓Line')) |> 
     mutate(wait_time_pre_check = case_when(wait_time_pre_check == "NoWait" ~ "0",
-                                           TRUE ~ wait_time_pre_check) |> 
-             as.numeric()) |> 
+                                           wait_time_pre_check == "" ~ NA,
+                                           TRUE ~ wait_time_pre_check), 
+           wait_time_pre_check = as.numeric(wait_time_pre_check)) |> 
+    suppressWarnings() |> 
     # DateTime
     mutate(datetime = lubridate::now(tzone = 'EST'),
            # Date
