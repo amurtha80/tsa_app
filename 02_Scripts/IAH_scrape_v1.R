568099946b19d8f3941faada57358f88812799d4
Andy Murtha <james.a.murtha@gmail.com>
2025-08-05 18:17:57 -0400

    Update code for script that is no longer used (comment out polite::bow() command)

--- 02_Scripts/IAH_scrape_v1.R
+++ 02_Scripts/IAH_scrape_v1.R
@@ -26,7 +26,7 @@ scrape_tsa_data_iah <- function() {
   
   # Define URL and initiate polite session
   url <- "https://www.fly2houston.com/iah/security"  # Update with the actual URL
-  session <- polite::bow(url)
+  # session <- polite::bow(url)
   options(chromote.headless = "new")
   
   # Scrape and parse data
